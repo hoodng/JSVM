ba4970ece5c154b064aa2109bcfa34cb353634c8
Hu Dong <winter.hu@gmail.com>
2013-01-22 21:58:13 +0800

    Format js.util.Locale

--- jsvm/src/js/util/Locale.js
+++ jsvm/src/js/util/Locale.js
@@ -1,9 +1,38 @@
 /**
- * Copyright (c) Jinfonet Inc. 2000-2012, All rights reserved.
- * 
- * @File: Locale.js
- * @Create: 2012/12/26 06:22:48
- * @Author: mingfa.pan@china.jinfonet.com
+
+ Copyright 2010-2011, The JSVM Project. 
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, 
+ are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice, 
+ this list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice, 
+ this list of conditions and the following disclaimer in the 
+ documentation and/or other materials provided with the distribution.
+ 
+ 3. Neither the name of the JSVM nor the names of its contributors may be 
+ used to endorse or promote products derived from this software 
+ without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ *
+ * Author: Pan mingfa
+ * Contact: jsvm.prj@gmail.com
+ * License: BSD 3-Clause License
+ * Source code availability: http://jzvm.googlecode.com
  */
 
 $package("js.util");
@@ -23,6 +52,27 @@ js.util.Locale = function(language, country){
     CLASS.__defined__ = true;
     
     var C = js.lang.Class, System = J$VM.System;
+
+
+	CLASS.getDateSymbols = function(locale){
+		var res, symbols;
+		if(locale && locale instanceof CLASS){
+			res = C.forName("js.text.resources." + locale.toString());
+			symbols = res ? res.dateSymbols : undefined;
+		}
+		
+		return symbols;
+	};
+
+	CLASS.getNumberSymbols = function(locale){
+		var res, symbols;
+		if(locale && locale instanceof CLASS){
+			res = C.forName("js.text.resources." + locale.toString());
+			symbols = res ? res.numrSymbols : undefined;
+		}
+		
+		return symbols;
+	};
 
     /**
      * Sets the language of current Locale. Language always be lower case. 
@@ -95,22 +145,3 @@ js.util.Locale = function(language, country){
     this._init.apply(this, arguments);
 };
 
-js.util.Locale.getDateSymbols = function(locale){
-    var res, symbols;
-    if(locale && locale instanceof js.util.Locale){
-        res = js.lang.Class.forName("js.text.resources." + locale.toString());
-        symbols = res ? res.dateSymbols : undefined;
-    }
-    
-    return symbols;
-};
-
-js.util.Locale.getNumberSymbols = function(locale){
-    var res, symbols;
-    if(locale && locale instanceof js.util.Locale){
-        res = js.lang.Class.forName("js.text.resources." + locale.toString());
-        symbols = res ? res.numrSymbols : undefined;
-    }
-    
-    return symbols;
-};
