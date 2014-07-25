4cfe6e5fac37ee839ab65b2c2f80ca640a07759e
Hu Dong <dong.hu@china.jinfonet.com>
2014-07-25 11:24:41 +0800

    modified:   jsre.js

--- jsvm/jsre.js
+++ jsvm/jsre.js
@@ -1,6 +1,6 @@
 (function(){
 
-    var _createXHR = function(){
+    var createXHR = function(){
         var PV = {
             progid: undefined,
             progids:["MSXML2.XMLHTTP.6.0",
@@ -29,23 +29,89 @@
         return xhr;
     };
 
-    var script = document.getElementById("j$vm");
-    if(script){
-        var srcpath = script.src || script.crs, p, xhr;
-        p = srcpath.lastIndexOf("/");
-        srcpath = srcpath.substring(0, p);
-        srcpath += "/jsre-core.jz?_="+(new Date()).getTime();
+    var cache = new (function(){
+        var local, session;
 
-        xhr = _createXHR();
+        this.setItem = function(key, value){
+            try{
+                local.removeItem(key);
+                local.setItem(key, value);
+            } catch (e1) {
+                try{
+                    session.removeItem(key);
+                    session.setItem(key, value);
+                } catch (e2) {
+                }
+            }
+        };
+
+        this.getItem = function(key){
+            var value;
+
+            try{
+                value = session.getItem(key);
+            } catch (e1) {
+            }
+
+            if(!value){
+                try{
+                    value = local.getItem(key);
+                } catch (e2) {
+                }
+            }
+
+            return value;
+        };
+
+        var _init = function(){
+            local  = self.localStorage;
+            session= self.sessionStorage;
+        }();
+
+    })();
+
+    var script = document.getElementById("j$vm");
+    if(!script) return;
+
+    var j$vm_home = script.src || script.crs, p, xhr, packages, srcpath;
+    p = j$vm_home.lastIndexOf("/");
+    j$vm_home = j$vm_home.substring(0, p);
+
+    // Load package.jz
+    srcpath = j$vm_home + "/package.jz";
+    xhr = createXHR();
+    xhr.open("GET", srcpath, false);
+    xhr.send(null);
+    packages = JSON.parse(xhr.responseText);
+
+    // Load jsre-core
+    srcpath = j$vm_home+"/jsre-core.jz";
+    var cached = cache.getItem("jsre-core.jz"), text;
+    if(cached){
+        cached = JSON.parse(cached);
+        if(cached.build === packages["jsre-core.jz"]){
+            text = cached.text;
+        }
+    }
+
+    if(text == undefined){
+        xhr = createXHR();
         xhr.open("GET", srcpath, false);
         xhr.send(null);
-
-        script = document.createElement("script");
-        p = document.getElementsByTagName("head")[0];
-        script.type = "text/javascript";
-        script.text = xhr.responseText;
-        p.appendChild(script);
-        p.removeChild(script);
+        text = xhr.responseText;
+        cache.setItem("jsre-core.jz", JSON.stringify({
+            build: packages["jsre-core.jz"],
+            text: text
+        }));
     }
+
+    self.j$vm_pkgversion = packages;
+
+    script = document.createElement("script");
+    p = document.getElementsByTagName("head")[0];
+    script.type = "text/javascript";
+    script.text = text;
+    p.appendChild(script);
+    p.removeChild(script);
 
 })();
