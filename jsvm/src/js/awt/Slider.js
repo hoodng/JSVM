1c4c58c1f93a29be8d3c7a3dcf0e24ac59114c86
hudong <hoodng@hotmail.com>
2016-04-01 00:22:37 +0800

    modified:   app/demo/graph.html 	modified:   jsvm/src/js/awt/Slider.js 	modified:   style/default/jsvm.css

--- jsvm/src/js/awt/Slider.js
+++ jsvm/src/js/awt/Slider.js
@@ -11,22 +11,14 @@
  */
 $package("js.awt");
 
-$import("js.awt.Bidirectional");
-
 /**
  * @param def:{
  *     className: 
  *     id:
  *     
- *     direction: 0: horizontal, 1: vertical
  *     type: 0: single, 1: range
  *     
  *     duration: number of seconds
- * 
- *     tracemouse: 0: slipper center trace mouse
- *         1: slipper endpoint trace mouse, and keep range no change
- *         3: slipper endpoint trace mouse, another endpoint is fixed, 
- *            the range will changed.
  *          
  * }
  */
@@ -69,8 +61,7 @@ js.awt.Slider = function(def, Runtime){
     thi$.setDataCount = function(count){
         
         //if( !Class.isNumber(count) || count <= 0 )
-        //throw "The data count must large than 0";
-        
+        //throw "The data count must large than 0";        
 
         this.datacount = count;
         _setMoverGrid.call(this, count);
@@ -160,13 +151,14 @@ js.awt.Slider = function(def, Runtime){
             if(offset0 !== 0 && p0 === 0){
                 p0 = 1;
             }
-            slipper.setUPosition(p0, null, 0x07);    
+            slipper.setPosition(p0, null, 0x04);
+            _updateTrack.call(this, p0, this.getBounds());
         }else{
-            var S = slipper.getSizeByRange(p1-p0);
-            slipper.setUBounds(p0, null, S.measure, undefined, 0x07);
+            var w = slipper.getSizeByRange(p1-p0);
+            slipper.setBounds(p0, null, w, null, 0x04);
         }
 
-        _layout.call(this, this.getUBounds(), doLayout !== false ? 1 :0);
+        _layout.call(this, this.getBounds(), doLayout !== false ? 1 :0);
 
     };
 
@@ -218,27 +210,6 @@ js.awt.Slider = function(def, Runtime){
             this.play(false);
         }
     };
-    /**
-      var _play = function(b, t0){
-      delete this.timer;
-      this.playing = true;
-
-      var slipper = this.slipper, 
-      c = this.getTrackLength(),
-      d = this.getDuration()*1000,
-      // b + v*T
-      p = b + (c/d)*(new Date().getTime()-t0);
-
-      p = p > c ? c : p;
-      this.setOffset(p/c, null, true);
-
-      if(p < c){
-      this.timer = _play.$delay(this, 10, b, t0);
-      }else{
-      this.play(false);
-      }
-      };
-      /**/
 
     /**
      * @see js.awt.Container
@@ -247,14 +218,14 @@ js.awt.Slider = function(def, Runtime){
         if($super(this)){
             this.slipper.doLayout(true);
             
-            var bounds = this.getUBounds();
+            var bounds = this.getBounds();
             
             if(this.offset){
                 // Adjust scale
                 var slipper = this.slipper, o = this.offset;
                 
-                this.trackLen = bounds.innerMeasure - 
-                    (slipper.offset0-slipper.offset1);
+                this.trackLen = bounds.innerWidth - 
+                    (slipper.offset0 - slipper.offset1);
                 
                 if(Class.isNumber(this.datacount) ||
                    Class.isBigInt(this.datacount)){
@@ -269,146 +240,30 @@ js.awt.Slider = function(def, Runtime){
             return true;
         }
         return false;
+        
     }.$override(this.doLayout);
     
     var _layout = function(bounds, fire){
         var slipper = this.slipper, off0, off1, p;
-        bounds = bounds || this.getUBounds();
         
-        _layout0.call(this, bounds);
+        bounds = bounds || this.getBounds();
+        
+        this.trackLen = bounds.innerWidth -
+            (slipper.offset0-slipper.offset1);
+        this.maxOffset= bounds.innerWidth -
+            slipper.getWidth();
+        this.offset = this.getOffset();
 
         this.isFirst = false;
         this.isLast  = false;
-        p = slipper.getStart();
+        p = slipper.getX();
         if(p == 0){
             this.isFirst = true;
         }else if(p >= this.maxOffset){
             this.isLast = true;
         }
-
-        if(typeof this.onSliderChanged == "function"){
-            var U = this._local;
-            off0 = slipper.getOffset0();
-            off1 = slipper.getOffset1();
-            
-            //new
-            //if(off0 != U.off0 || off1 != U.off1 || (fire & 0x01) != 0){
-            this.onSliderChanged(fire);
-            U.off0 = off0; U.off1 = off1;
-            //}
-        }
-        
-        // Resume paused play
-        if(fire == 1 && this.paused == true){
-            this.paused = false;
-            this.play(true);
-        }
-    };
-
-    var _layout0 = function(D){
-        var slipper = this.slipper, 
-            slipperS = (D.innerPMeasure - slipper.getPMeasure())*0.5,
-            track0 = this._track0, track = this._track;
-        debugger;
-        slipper.setUPosition(null, slipperS);
-        this.trackLen = D.innerMeasure - (slipper.offset0-slipper.offset1);
-        this.maxOffset= D.innerMeasure - slipper.getMeasure();
-        this.offset = this.getOffset();
-    };
-
-    var _onmousedown = function(e){
-//        debugger;
     };
     
-    /**
-     * @see js.awt.Movable
-     */
-    thi$.isMoverSpot1 = function(el, x, y){
-        
-        if(this.isPlaying()) {
-            this.paused = true;
-
-            this.play(false);
-        }
-
-        if(el.className.indexOf("resizer") != -1) return false;
-
-        this.moveSlipper = false;
-        
-        var slipper = this.slipper;
-        if(slipper.contains(el, true)) {
-            // If mousedown in on the slipper, do we need 
-            // stop play ?  
-            this.paused = false;
-            this.moveSlipper = true;
-            return true;
-        }
-
-        var xy = this.relative({x:x, y:y}), bounds = this.getUBounds(), 
-            offset0 = slipper.getOffset0(), offset1 = slipper.getOffset1(),
-            offset, max, v, m, pm, grid = this.def.mover.grid, 
-            needLayout = true;
-
-        if(this.isHorizontal()){
-            xy.m  = grid*Math.round(xy.x/grid);
-            xy.pm = xy.y;
-        }else{
-            xy.m  = grid*Math.round(xy.y/grid);
-            xy.pm = xy.x;
-        }
-        
-        switch(this.def.tracemouse){
-            case 0:
-            offset = Math.floor(slipper.offset0 + (offset1-offset0)/2);
-            max = bounds.innerMeasure - slipper.getMeasure();
-            //m = xy.m - offset;
-            m = xy.m;
-            m = m < 0 ? 0 : (m > max ? max : m);
-            slipper.setUPosition(m, null, 7);
-            break;
-            case 1:
-            if(xy.m < offset0){
-                m = xy.m - slipper.offset0;
-                m = m < 0 ? 0 : m;
-            }else if(xy.m > offset1){
-                max = bounds.innerMeasure - slipper.getMeasure();
-                m = xy.m - slipper.getMeasure() - slipper.offset1;
-                m = m > max ? max : m;
-            }else{
-                m = null;
-            }
-            slipper.setUPosition(m, null, 7);
-            break;
-            case 3:
-            if(xy.m < offset0){
-                m = xy.m - slipper.offset0;
-                m = m < 0 ? 0 : m;
-                v = slipper.getSizeByRange(offset1-m-slipper.offset0).measure;
-            }else if(xy.m > offset1){
-                m = null;
-                max = bounds.innerMeasure - (-slipper.offset1);
-                xy.m = xy.m > max ? max : xy.m;
-                v = slipper.getSizeByRange(xy.m - offset0).measure;
-            }else{
-                m = null;
-                v = undefined;
-            }
-            slipper.setUBounds(m, null, v, undefined, 7);
-            break;
-            default:
-            needLayout = false;
-            break;
-        }
-
-        if(needLayout){
-            _layout.call(this, bounds, 1);    
-        }
-        
-        
-        return true;
-    };
-    /**/
-
     /**
      * If data count < track's pixel, the slipper should be 
      * snaped to grid
@@ -424,30 +279,41 @@ js.awt.Slider = function(def, Runtime){
         }
     };
 
-    var _onmoving = function(e){
-        /*
-        var fire = (this.moveSlipper == true &&
-                    e.getType() == "mouseup") ? 1 : 0;
-        _layout.call(this, this.getUBounds(), fire);
-        */
-    };
-
     var _onMoving = function(e){
-        var data = e.getData(), bounds = this.getBounds(),
-            track = this._track, measure;
+        var data = e.getData();
         if(this.isSingle()){
-            track.style.borderLeftWidth = data.nx+"px";
-            track.bounds = null;
-            DOM.setSize(track, bounds.innerWidth);
+            _updateTrack.call(this, data.nx, this.getBounds());
         }
-        e.cancelBubble();
-    };
-
-    var _onMoveEnd = function(e){
         
         e.cancelBubble();
     };
 
+    var _updateTrack = function(x, bounds){
+        var track = this._track;
+        track.style.borderLeftWidth = x+"px";
+        track.bounds = null;
+        DOM.setSize(track, bounds.innerWidth);
+    };
+
+    var _onMoveEnd = function(e){
+        this.offset = this.getOffset();
+
+        if(Class.isFunction(this.onSliderChanged)){
+            var U = this._local, slipper = this.slipper;
+            U.off0 = slipper.getOffset0();
+            U.off1 = slipper.getOffset1();
+            this.onSliderChanged.$delay(this, 1);
+        }
+        e.cancelBubble();
+    };
+
+    var _onTraceMouse = function(e){
+        var xy = e.eventXY, bounds = this.getBounds();
+        xy = DOM.relative(xy.x, xy.y, bounds);
+        
+        e.cancelBubble();
+    };
+    
     var _createElements = function(){
         var R = this.Runtime(), uuid = this.uuid(),
             track0, track,slipper;
@@ -472,14 +338,8 @@ js.awt.Slider = function(def, Runtime){
                 id: [uuid,"slipper"].join("-"),
                 stateless: true,
                 movable: true,
-                mover:{
-                    grid:1,
-                    bt:1,
-                    br:1,
-                    bb:1,
-                    bl:1,
-                    freedom: this.isHorizontal() ? 1:2
-                }
+                mover:{grid:1, bt:1, br:1, bb:1, bl:1, freedom: 1,
+                       longpress: 10}
             },R);
 
         slipper.setPeerComponent(this);
@@ -487,10 +347,21 @@ js.awt.Slider = function(def, Runtime){
     };
     
     thi$.destroy = function(){
-        this.detachEvent(Event.W3C_EVT_MOUSE_DOWN, 4, this, _onmousedown);
-        MQ.cancel(this.slipper.getMovingMsgType(), this, _onmoving);
-        MQ.cancel(this.slipper.getSizingMsgType(), this, _onmoving);
+        
+        this.slipper.$destroy();
+        this.slipper = null;
+        this._track = null;
+        this._track0 = null;
+
+        this.detachEvent("elementMoving", 4, this, _onMoving);
+        this.detachEvent("elementMoveEnd",4, this, _onMoveEnd);
+        this.detachEvent("elementResizeEnd", 4, this, _onMoveEnd);
+        /*
+        this.detachEvent(Event.W3C_EVT_MOUSE_DOWN,
+                         4, this, _onTraceMouse);*/
+        
         $super(this);
+        
     }.$override(this.destroy);
 
     thi$._init = function(def, Runtime){
@@ -498,41 +369,24 @@ js.awt.Slider = function(def, Runtime){
         
         def.classType = def.classType || "js.awt.Slider";
         def.type   = Class.isNumber(def.type) ? def.type : 0;
-        def.direction = Class.isNumber(def.direction) ? def.direction : 0;
         def.duration = Class.isNumber(def.duration) ? def.duration : 1;
-        def.tracemouse = Class.isNumber(def.tracemouse) ? def.tracemouse : 0;
         
         $super(this);
         
         _createElements.call(this);
 
-        var M = this.def, slipper = this.slipper, uuid = this.uuid();
-        
-        slipper.getMovingMsgRecvs =
-            slipper.getSizingMsgRecvs = function(){
-                return [uuid];
-            };
-
-        MQ.register(this.slipper.getMovingMsgType(), this, _onmoving);
-        
-        if(!this.isSingle()){
-        //    MQ.register(this.slipper.getSizingMsgType(), this, _onmoving);
-        }
-        else{
-            if(M.tracemouse == 1 || M.tracemouse == 3){
-                M.tracemouse = 0;
-            }
-        }
-
         this.attachEvent("elementMoving", 4, this, _onMoving);
         this.attachEvent("elementMoveEnd",4, this, _onMoveEnd);
-        this.attachEvent(Event.W3C_EVT_MOUSE_DOWN, 4, this, _onmousedown);
+        this.attachEvent("elementResizeEnd", 4, this, _onMoveEnd);
+        /*
+        this.attachEvent(Event.W3C_EVT_MOUSE_DOWN,
+                         4, this, _onTraceMouse);*/
 
     }.$override(this._init);
     
     this._init.apply(this, arguments);
 
-}.$extend(js.awt.Component).$implements(js.awt.Bidirectional);
+}.$extend(js.awt.Component);
 
 /**
  * @param def:{
@@ -562,62 +416,41 @@ js.awt.Slipper = function(def, Runtime){
      * Return the offset0 position of this slipper
      */
     thi$.getOffset0 = function(){
-        return this.getStart() + this.offset0;
+        return this.getX() + this.offset0;
     };
 
     /**
      * Return the offset1 position of this slipper
      */
     thi$.getOffset1 = function(){
-        return this.getStart() + this.getMeasure() - (-this.offset1);
+        return this.getX() + this.getWidth() + this.offset1;
+    };
+
+    thi$.getWidth = function(){
+        return this.getBounds().offsetWidth;
     };
 
     /**
      * @see js.awt.Container
      */    
     thi$.doLayout = function(force){
-        debugger;
-        if(this._local.doneLayout !== true &&
-           $super(this)){
-
-            _layout.call(this, this.getUBounds());
+        var bounds, mbp, w;
+        if($super(this)){
+            bounds = this.getBounds();
+            if(this.isSingle()){
+                this.offset0 = bounds.offsetWidth/2;
+                this.offset1 = -this.offset0;
+            }else{
+                mbp = bounds.MBP;
+                w = DOM.getBounds(this.ctrl0).offsetWidth;
+                this.offset0 = mbp.borderLeftWidth + w;
+                this.offset1 = -(mbp.borderRightWidth + w);
+                
+            }
             return true;
         }
         return false;
     }.$override(this.doLayout);
-
-    var _layout = function(D){
-        var b = this.isHorizontal(), d, w;
-        if(this.isSingle()){
-            this.offset0 = D.measure/2; 
-            this.offset1 = -this.offset0;
-        }else{
-            d = DOM.getBounds(this.ctrl0);
-            w = b ? d.width : d.height;
-            this.offset0 = D.MBP.borderM0 + w;
-            this.offset1 = 0 - D.MBP.borderM1 - w;
-
-            if(this.def.miniSize == undefined){
-                // Keep 1px for ranger ?
-                this.setUMinimumSize(D.MBP.BM + 2*w + 1, D.pmeasure);
-            }
-        }
-        this._local.doneLayout = true; 
-    };
-    
-    /**
-     * @see js.awt.MoveObject
-     */
-    thi$.getMovingMsgType = function(){
-        return "js.awt.event.SliderMovingEvent";    
-    };
-
-    /**
-     * @see js.awt.SizeObject
-     */
-    thi$.getSizingMsgType = function(){
-        return "js.awt.event.SliderSizingEvent";
-    };
 
     /**
      * @see js.awt.Resizable
@@ -632,57 +465,37 @@ js.awt.Slipper = function(def, Runtime){
     }.$override(this.getSizeObject);
 
     thi$.getSizeByRange = function(range){
-        var ret;
+        var D = this.getBounds(), d;
         if(this.isSingle()){
-            ret = this.getUSize();
-        }else{
-            var d = DOM.getBounds(this.ctrl0), 
-                D = this.getBounds(), 
-                width = range + 2*d.width + D.MBP.BW,
-                height= range + 2*d.height+ D.MBP.BH;
-            if(this.isHorizontal()){
-                ret = {
-                    width:  width,
-                    height: D.height,
-
-                    measure:width,
-                    pmeasure: D.height
-                };
-            }else{
-                ret = {
-                    width:  D.width,
-                    height: height,
-
-                    measure:height,
-                    pmeasure: D.width
-                };
-            }
+            return D.offsetWidth
         }
 
-        return ret;
+        d = DOM.getBounds(this.ctrl0);
+        return range + 2*d.width + D.MBP.BW
+
     };
+
+    var w = 3, w2 = w*2;
 
     thi$.showResizeCapture = function(e){
         var xy = e.eventXY(), bounds = this.getBounds(),
             idxes = DOM.offsetIndexes(xy.x, xy.y, bounds),
-            b = this.isHorizontal(), idx = idxes[2],
-            d, spot, ret = false;
+            idx = idxes[2], d, spot, ret = false;
 
         if(idx < 3){
             d = DOM.getBounds(this.ctrl0);
             bounds = {
-                x : d.absX - 2, y: d.absY - 2,
-                width: d.width + 4, height: d.height + 4
+                x : d.absX - w, y: d.absY - w,
+                width: d.width + w2, height: d.height + w2
             }
-            spot = b ? 1 : 7;
-            
+            spot = 1;
         }else if(idx > 3 && idx < 8){
             d = DOM.getBounds(this.ctrl1);
             bounds = {
-                x : d.absX - 2, y: d.absY - 2,
-                width: d.width + 4, height: d.height + 4
+                x : d.absX - w, y: d.absY - w,
+                width: d.width + w2, height: d.height + w2
             }
-            spot = b ? 5 : 3;
+            spot = 5;
         }
 
         if(d){
@@ -692,34 +505,42 @@ js.awt.Slipper = function(def, Runtime){
 
         return ret;
     };
+
+    /*
+    thi$.showMoveCapture = function(e){
+        if(this.isSingle()){
+            var d = this.getBounds();
+            DOM.showMouseCapturer({
+                x: d.absX - w , y: d.absY - w,
+                width: d.width + w2,
+                height:d.height+ w2
+            }, this.uuid(), 8);
+            
+            return true;
+        }
+        return false;
+    };*/
     
     var _createElements = function(){
-        var view = this.view, uuid, className, d, ctrl0, ctrl1;
+        var view = this.view, uuid, className, ctrl0, ctrl1;
         if(!this.isSingle()){
             className = this.className;
             uuid = this.uuid();
-            //d = this.isHorizontal() ? "--h":"--v";
-            d = "";
             
             this.ctrl0 = ctrl0 = DOM.createElement("DIV");
             ctrl0.id = [uuid, "ctrl0"].join("-");
             ctrl0.uuid = uuid;
-            ctrl0.className = [className, "_ctrl0",d].join("");
+            ctrl0.className = [className, "_ctrl0"].join("");
             view.appendChild(ctrl0);
 
             this.ctrl1 = ctrl1 = DOM.createElement("DIV");
             ctrl1.id = [uuid, "ctrl1"].join("-");
             ctrl1.uuid = uuid;
-            ctrl1.className = [className,"_ctrl1",d].join("");
+            ctrl1.className = [className,"_ctrl1"].join("");
             view.appendChild(ctrl1);
         }
     };
     
-    thi$.destroy = function(){
-        MQ.cancel(this.getSizingMsgType(), this, _onsizing);
-        $super(this);
-    }.$override(this.destroy);
-
     thi$._init = function(def, Runtime){
         if(def == undefined) return;
         
@@ -731,7 +552,7 @@ js.awt.Slipper = function(def, Runtime){
         if(def.type !== 0){
             // Range type
             def.resizable= true;
-            def.resizer = def.direction === 0 ? 0x22 : 0x88;
+            def.resizer = 0x22;
         }
         
         $super(this);
@@ -742,5 +563,5 @@ js.awt.Slipper = function(def, Runtime){
     
     this._init.apply(this, arguments);
     
-}.$extend(js.awt.Component).$implements(js.awt.Bidirectional);
+}.$extend(js.awt.Component);
 
